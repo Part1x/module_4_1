b10aadbc48b68fc8395fe97dc61cc220d41d7e4c
Part1x <part1xtube@gmail.com>
2024-09-06 18:57:29 +0300

    Add files via upload

--- fake_math.py
+++ fake_math.py
@@ -2,5 +2,4 @@ def divide(first, second):
     if second == 0:
         print('Ошибка!')
     else:
-        return first // second
-
+        return first / second
--- true_math.py
+++ true_math.py
@@ -3,4 +3,4 @@ def divide(first, second):
     if second == 0:
         return inf
     else:
-        return first // second
+        return first / second
